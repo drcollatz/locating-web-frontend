52d4a90f082dd34b8daee00d2930a5fbb3b4895d
florensh <florensh@gmx.de>
2016-08-05 01:21:03 +0200

    changed calendar range

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,7 +1,7 @@
 const AppSettings = {
   appTitle: 'Locating',
-  apiUrl: 'https://young-beach-90165.herokuapp.com',
-  // apiUrl: '',
+  // apiUrl: 'https://young-beach-90165.herokuapp.com',
+  apiUrl: '',
   currentVisitorTimeRange: 20 * 60
 };
 
