6f6a3006c93999e9ea2f811010fecc48c30072a9
florensh <florensh@gmx.de>
2016-09-26 22:00:10 +0200

    design improvements

--- app/js/constants.js
+++ app/js/constants.js
@@ -2,7 +2,8 @@ const AppSettings = {
   appTitle: 'Locating',
   // apiUrl: 'https://young-beach-90165.herokuapp.com',
   // apiUrl: 'http://collalize.herokuapp.com',
-  apiUrl: 'http://localhost:8080',
+  // apiUrl: 'http://localhost:8080',
+  apiUrl: '',
   currentVisitorTimeRange: 20 * 60,
   calendarColors: ['#86BBD8', '#F2E94E', '#F9AB55', '#B08EA2']
 };
